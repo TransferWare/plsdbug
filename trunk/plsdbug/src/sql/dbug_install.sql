9b83b1bd7794335d1542a12775c834cb18832098
gpaulissen <gpaulissen@c1c61559-3a1e-0410-b1f8-a6e2dc18b5fa>
2008-07-28 12:05:14 +0000

    Updated install.sql

--- trunk/plsdbug/src/sql/dbug_install.sql
+++ trunk/plsdbug/src/sql/dbug_install.sql
@@ -3,17 +3,31 @@ REMARK
 REMARK  $HeadURL$
 REMARK
 
+prompt @@dbug.pks
 @@dbug.pks
+prompt @@dbug_call_obj_t.typ
 @@dbug_call_obj_t.typ
+prompt @@dbug_call_tab_t.typ
 @@dbug_call_tab_t.typ
+prompt @@dbug_obj_t.typ
 @@dbug_obj_t.typ
+prompt @@dbug.pkb
 @@dbug.pkb
+prompt @@dbug_obj_t.tyb
 @@dbug_obj_t.tyb
+prompt @@dbug_plsdbug_obj_t.typ
 @@dbug_plsdbug_obj_t.typ
+prompt @@dbug_plsdbug_obj_t.tyb
 @@dbug_plsdbug_obj_t.tyb
+prompt @@dbug_log4plsql_obj_t.typ
 @@dbug_log4plsql_obj_t.typ
+prompt @@dbug_log4plsql_obj_t.tyb
 @@dbug_log4plsql_obj_t.tyb
+prompt @@dbug_trigger.pls
 @@dbug_trigger.pls
+prompt @@dbug_plsdbug.pls
 @@dbug_plsdbug.pls
+prompt @@dbug_dbms_output.pls
 @@dbug_dbms_output.pls
+prompt @@dbug_log4plsql.pls
 @@dbug_log4plsql.pls
