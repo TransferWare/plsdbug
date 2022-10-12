2d184d05885b762553c4198735a126e5732768f7
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-10-12 11:32:44 +0200

    Installing database objects enhanced.

--- src/prog/install.sql
+++ src/prog/install.sql
@@ -3,3 +3,9 @@ whenever oserror exit failure
 
 prompt @@dbug_plsdbug.pls
 @@dbug_plsdbug.pls
+
+prompt show errors package dbug_plsdbug
+show errors package dbug_plsdbug
+
+prompt show errors package body dbug_plsdbug
+show errors package body dbug_plsdbug
