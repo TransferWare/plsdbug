9b1e5a02f477bde5f4ba31b4703f8b71b57edaab
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-04-02 14:38:29 +0200

    Removing tests.

--- test/benchmark.sql
+++ test/benchmark.sql
@@ -3,29 +3,8 @@ set feedback off
 set trimspool on
 set verify off
 
-declare
-  procedure doit
-  is
-  begin
-    dbug.enter( 'doit' );
-    dbug.leave;
-  end;
 begin
-  dbug.activate( '&&2' );
-  case upper('&&2')
-    when 'PLSDBUG'
-    then
-      dbug_plsdbug.init( '&&3' );
-    else
-      null;
-  end case;
-  dbug.enter( 'main' );
-  for v_nr in 1..&&1
-  loop
-    doit;
-  end loop;
-  dbug.leave;
-  dbug.done;
+  ut_dbug.ut_benchmark('&&1', '&&2', '&&3');
 end;
 /
 
--- test/factorial.sql
+++ test/factorial.sql
@@ -4,36 +4,7 @@ set trimspool on
 set verify off
 set linesize 1000 trimspool on
 
-declare
-	l_dbug_method constant varchar2(100) := '&&2';
-	l_plsdbug_options constant varchar2(100) := '&&3';
-
-        function
-        factorial (i_value in integer)
-        return  integer
-        is
-                v_value integer := i_value;
-        begin
-                dbug.enter( 'factorial' );
-                dbug.print( 'find', 'find %s factorial', v_value );
-                if (v_value > 1) 
-                then
-                        v_value := v_value * factorial( v_value-1 );
-                end if;
-                dbug.print( 'result', 'result is %s', v_value );
-                dbug.leave;
-                RETURN (v_value);
-        end;
 begin
-	case upper(l_dbug_method)
-          when 'PLSDBUG'
-          then
-		dbug.activate( l_dbug_method );
-        	dbug_plsdbug.init( l_plsdbug_options );
-	  else
-		dbug.activate( l_dbug_method );
-	end case;
-        dbms_output.put_line( factorial( &&1 ) );
-        dbug.done;
+  ut_dbug.ut_factorial('&&1', '&&2');
 end;
 /
