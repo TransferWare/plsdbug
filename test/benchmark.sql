38b2fe0f6ce63febf4673d197d65cc061732c2f2
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-04-03 20:49:19 +0200

    test OK

--- test/benchmark.sql
+++ test/benchmark.sql
@@ -1,7 +1,7 @@
-set serveroutput on size 1000000
+set serveroutput on size unlimited format trunc
 set feedback off
 set trimspool on
-set verify off
+set verify on
 
 begin
   ut_dbug.ut_benchmark('&&1', '&&2', '&&3');
--- test/leave.sql
+++ test/leave.sql
@@ -5,18 +5,20 @@ define userid = '&&1'
 define dbug_method = '&&2'
 define dbug_options = '&&3'
 
-define termout_off = off
+define termout_off = on
 
 set termout &&termout_off
 connect &&userid
 
 set feedback off
-
+set linesize 1000 trimspool on
+set serveroutput on size unlimited format trunc
 whenever sqlerror exit failure
 
 set termout &&termout_off
 
 begin
+  dbug.activate('dbms_output');
   ut_dbug.ut_leave('&&dbug_method', '&&dbug_options');
 end;
 /
